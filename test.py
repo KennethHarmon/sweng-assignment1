a1cfe387685cbd5bfd57a5990b96028fcc2bdb4d
Louis Clarke <98478206+louisclarkee@users.noreply.github.com>
2022-10-08 16:53:00 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,8 @@
-def test_uppercase():
+from calculator.py import evaluate_postfix_expression
+
+def initial_test():
     assert "loud noises".upper() == "LOUD NOISES"
+    
+def test_postfixevaluation():
+    assert evaluate_postfix_expression([2, 2, '+']) == 4
+
